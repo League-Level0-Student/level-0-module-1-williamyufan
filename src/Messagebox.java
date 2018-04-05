4ba2c89d898f4ab310ad860068155b882061431a
arkangel6 <league@wts7.attlocal.net>
2018-04-04 18:26:44 -0700

    day 2 level 0

--- src/Messagebox.java
+++ src/Messagebox.java
@@ -3,6 +3,17 @@ import javax.swing.JOptionPane;
 public class Messagebox {
 public static void main(String[] args) {
 	String Password= "0090";
-	JOptionPane.showMessageDialog(null, "Tell me one of your Secrets!");
+	String Secret=JOptionPane.showInputDialog(null, "Tell me one of your Secrets!");
+	String Answer=JOptionPane.showInputDialog(null, "What is the password");
+if(Answer.equals(Password)){	
+
+JOptionPane.showMessageDialog(null, Secret);
+}
+else {
+	JOptionPane.showMessageDialog(null, "INVADER!");
+
+
+
+}
 }
 }
--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,24 +1,38 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
-		// 1. Make a variable to hold the score
+		int score = 0;
 
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+		String Riddle = JOptionPane.showInputDialog(null, "The more you take, the more you leave behind. What am I?");
 
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		if (Riddle.equals("footsteps")) {
 
-		// 5. Otherwise, say "wrong" and tell them the answer
+			score++; 
+			
+			JOptionPane.showMessageDialog(null, "Correct");
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
 
-		// 6. Add some more riddles
+		}
+		JOptionPane.showMessageDialog(null, "1");
 
-		// 2. Make a pop up to show the score.
-		
+		String Riddle2 = JOptionPane.showInputDialog(null, "What room do ghosts avoid?");
+
+		if (Riddle.equals("livingroom")) {
+
+			score++;
+			JOptionPane.showMessageDialog(null, "Correct");
+		}
+
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+		}
+		JOptionPane.showMessageDialog(null, "2");
 	}
 }
-
